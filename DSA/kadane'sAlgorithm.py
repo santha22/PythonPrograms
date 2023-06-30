350c3bb71e5fb992b7b804965da5bb5ca53d9b4b
Savara Santha kumar <134350271+santha22@users.noreply.github.com>
2023-06-30 23:28:08 +0530

    Update kadane'sAlgorithm.py

--- DSA/kadane'sAlgorithm.py
+++ DSA/kadane'sAlgorithm.py
@@ -1,14 +1,16 @@
 
 def kadaneAlgo(a):
-    sum = 0
-    maxA = a[0]
-    for i in range(len(a)):
-        sum = sum + a[i]
-        [i]
-        maxA = max(maxA,sum)
-        if sum < 0:
-            sum = 0
-    return maxA
+    N=len(a)
+    curSum = 0
+        msf=arr[0]
+        for i in range(N):
+            curSum+=arr[i]
+            if msf < curSum:
+                msf = curSum
+            if curSum < 0:
+                curSum = 0
+            
+        return msf
 
 
 A = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
