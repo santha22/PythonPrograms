77f92a6c83fe8cd51d56f18f0eb5bf64e3e8a481
Savara Santha kumar <134350271+santha22@users.noreply.github.com>
2023-07-07 21:02:24 +0530

    Binary Search Tree

---
+++ DSA/BinarySearchTreeString.py
@@ -0,0 +1,64 @@
+class BinarySearchTreeNode:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def add_child(self, data):
+        if data == self.data:
+            return
+        
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+        
+    def inoder_traversal(self):
+        elements = []
+
+        if self.left:
+            elements += self.left.inoder_traversal()
+        
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.inoder_traversal()
+
+        return elements
+    
+    def search(self, val):
+        if self.data == val:
+            return True
+        
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+            
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+if __name__ == "__main__":
+    countries = ["India", "Pakistan", "Germany", "USA", "China","Inida", "UK", "USA"]
+    country_tree = build_tree(countries)
+
+    print("UK is in the list? ", country_tree.search("UK"))
+    print("Sweden is in the list? ", country_tree.search("Sweden"))
+   
---
+++ DSA/binarySearchTree.py
@@ -0,0 +1,68 @@
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return 
+        
+        if data < self.data:
+            # add data to left subtree
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def inoder_traversal(self):
+        elements = []
+        
+        # visit left tree
+        if self.left:
+            elements += self.left.inoder_traversal()
+        
+        # visit base node
+        elements.append(self.data)
+
+        # visit right tree
+        if self.right:
+            elements += self.right.inoder_traversal()
+
+        return elements
+    
+    def search(self, val):
+        if self.data == val:
+            return True
+        
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+            
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+    
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+if __name__ == '__main__':
+    numbers = [17, 4, 1, 20, 9, 23, 18, 34]
+    numbers_tree = build_tree(numbers)
+    print(numbers_tree.inoder_traversal())
+    print(numbers_tree.search(9))
+   
