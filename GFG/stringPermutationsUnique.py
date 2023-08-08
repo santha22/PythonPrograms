fc055b660b5ad1c02d71c9c14aed0ea4ddbaad87
Savara Santha kumar <134350271+santha22@users.noreply.github.com>
2023-08-08 22:46:59 +0530

    Update the code using list and freq

--- GFG/stringPermutationsUnique.py
+++ GFG/stringPermutationsUnique.py
@@ -1,4 +1,4 @@
-from itertools import permutations
+"""from itertools import permutations
 class Solution:
     def permutations(self, s, i, n, result):
         if (i == n - 1):
@@ -18,7 +18,31 @@ class Solution:
         self.permutations(list(s), 0, len(s), result)
         result = list(set(result))
         result.sort()
-        return result
+        return result"""
+
+class Solution:
+    def recurPermute(self,s,ds,ans,freq):
+        if(len(ds) == len(s)):
+            ans.append(''.join(ds))
+            return
+        for i in range(len(s)):
+            if(freq[i]==0):
+                freq[i] = 1
+                ds.append(s[i])
+                self.recurPermute(s,ds,ans,freq)
+                ds.pop()
+                freq[i] = 0
+    
+    
+    def find_permutation(self,s):
+        ans = []
+        ds = []
+        freq = [0]*len(s)
+        self.recurPermute(s,ds,ans,freq)
+        ans = list(set(ans))
+        ans.sort()
+        return ans
+    
 
 if __name__ == "__main__":
     t = int(input())
