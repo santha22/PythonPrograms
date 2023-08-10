e8aa08b45337b4960ef1ad8ae24169a7d5072d26
Savara Santha kumar <134350271+santha22@users.noreply.github.com>
2023-08-10 10:13:01 +0530

    Update longestCommonSubsequence

--- GFG/longestCommonSubsequence.py
+++ GFG/longestCommonSubsequence.py
@@ -5,8 +5,8 @@ class Solution:
             dp[i][0] = 0
         for j in range(y+1):
             dp[0][j] = 0
-        for i in range(x+1):
-            for j in range(y+1):
+        for i in range(1,x+1):
+            for j in range(1,y+1):
                 if(s1[i-1]==s2[j-1]):
                     dp[i][j] = 1 + dp[i-1][j-1]
                 else:
