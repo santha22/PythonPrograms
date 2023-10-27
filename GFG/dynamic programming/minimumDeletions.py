e8ec45df4d89429d5a153b35d566d03645f11626
Savara Santha kumar <134350271+santha22@users.noreply.github.com>
2023-10-27 15:21:12 +0530

    minimumDeletions.py

---
+++ GFG/dynamic programming/minimumDeletions.py
@@ -0,0 +1,20 @@
+class Solution:
+    
+    def minimumNumberOfDeletions(self,s):
+        # code here 
+        s1 = s[::-1]
+        n,m = len(s), len(s1)
+        
+        dp = [[0] * (m + 1) for i in range(n + 1)]
+        
+        for ind in range(1, n + 1):
+            for ind1 in range(1, m + 1):
+                if s[ind - 1] == s1[ind1 - 1]:
+                    dp[ind][ind1] = 1 + dp[ind - 1][ind1 - 1]
+                    
+                else:
+                    dp[ind][ind1] = max(dp[ind - 1][ind1], dp[ind][ind1 - 1])
+        
+        res = dp[n][m]
+        
+        return n - res
