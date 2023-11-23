dc0f76a87b19108c1541c6913ca1b37182ba38db
Savara Santha kumar <134350271+santha22@users.noreply.github.com>
2023-11-23 18:51:17 +0530

    reverseWords.py

--- GFG/string/reverseWords.py
+++ GFG/string/reverseWords.py
@@ -3,13 +3,28 @@ class Solution:
     #Function to reverse words in a given string.
     def reverseWords(self,s):
         # code here 
-        ans = s.split(".")
-        n = len(ans)
-        ans1 = []
-        for i in range(n - 1, 0, -1):
+        i, j = 0, 0
+        word = ""
+        res = ""
+        n = len(s)
+        while i < n:
+            while i < n and s[i] == ".":
+                i += 1
+                
+            if i >= n:
+                break 
             
-            ans1.append(ans[i])
+            j = i + 1
+            while j < n and s[j] != ".":
+                j += 1
+                
+            word = s[i: j]
+            if len(res) == 0:
+                res = word 
+                
+            else:
+                res = word + "." + res 
+                
+            i = j + 1
             
-        ans1.append(ans[0])
-            
-        return ".".join(ans1)
+        return res
